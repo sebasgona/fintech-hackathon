7031dce89e730c78f357575149f4858ced54e9b7
sebasgona <sebastian.gon.a@gmail.com>
2018-01-27 15:30:48 -0600

    PWA with drawer navigation bar, avatar and add button

--- sinf/src/App.vue
+++ sinf/src/App.vue
@@ -1,20 +1,72 @@
 <template>
-  <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Sinf.</span>
+      </div>
     </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <router-view></router-view>
+    <div class="mdl-layout__drawer">
+  
+
+    <v-toolbar flat class="transparent">
+      <v-list class="pa-0">
+        <v-list-tile avatar>
+          <v-list-tile-avatar>
+            <img style=border-radius:50%; height=100; width=100; src="https://media-exp2.licdn.com/mpr/mpr/shrinknp_200_200/AAIAAQDGAAwAAQAAAAAAAAwlAAAAJDY3YzNmMmQyLWIyNGEtNDQyZS04NzM1LWFjOTUxODUxOWMxZQ.jpg" />
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>John Leider</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
+
+    <v-list class="pt-0" dense>
+      <v-divider></v-divider>
+      <v-list-tile v-for="item in items" :key="item.title" @click="">
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+
+      <nav class="mdl-navigation">
+        <router-link class="mdl-navigation__link" to="/" @click.native="hideMenu">Home</router-link>
+        <router-link class="mdl-navigation__link" to="/detail-ine" @click.native="hideMenu">Ver INE</router-link>
+        <router-link class="mdl-navigation__link" to="/detail-passport" @click.native="hideMenu">Ver Pasaporte</router-link>
+        <router-link class="mdl-navigation__link" to="/cerrar-sesion" @click.native="hideMenu">Cerrar Sesión</router-link>
+      </nav>
+    </div>
+    <main class="mdl-layout__content">
+      <div class="page-content">
+        <router-view></router-view>
+      </div>
     </main>
   </div>
 </template>
 
 <script>
+
+require('material-design-lite')
+
 export default {
-  name: 'app'
+  name: 'app',
+  methods: {
+    hideMenu: function () {
+      document.getElementsByClassName('mdl-layout__drawer')[0].classList.remove('is-visible')
+      document.getElementsByClassName('mdl-layout__obfuscator')[0].classList.remove('is-visible')
+    }
+  }
 }
 </script>
+
+<style>
+  @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+  @import url('https://code.getmdl.io/1.2.1/material.blue-red.min.css');
+</style>
 
 <style>
 body {
--- sinf/src/router/index.js
+++ sinf/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import HomeView from '@/components/HomeView'
+// import CameraView from '@/components/CameraView'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Home',
+      component: HomeView
     }
   ]
 })
